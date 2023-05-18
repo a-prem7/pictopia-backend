75bc237554e5c984188ce61228a13a1075cab328
amal-prem <aldamal.prem@gmail.com>
2023-05-18 08:27:20 -0400

    adds blogschema model

---
+++ models/BlogSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
