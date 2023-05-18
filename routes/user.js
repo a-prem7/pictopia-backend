40e0d088846f90cb9334c858c3212dafffa1c06c
amal-prem <aldamal.prem@gmail.com>
2023-05-17 21:50:23 -0400

    adds user register and login

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,13 @@ const UserRouter = express.Router();
 UserRouter.use(express.json());
 
 UserRouter.get("/", async (req, res) => {
-  await User.find();
+  await User.find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json({ error: err });
+    });
 });
 
 UserRouter.post("/register", async (req, res) => {
@@ -14,8 +20,20 @@ UserRouter.post("/register", async (req, res) => {
     const user = await User.create({ name, email, password });
     res.json({ msg: "user registered successfully", user: user });
   } catch (error) {
-    res.json({ msg: error });
+    res.json({ error: error });
   }
 });
 
+UserRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existUser = await User.findOne({ email, password });
+    if (!existUser) {
+      res.status(404).json({ msg: "user not found" });
+    }
+    res.status(201).json({ msg: "user logged in", user: existUser });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
 export default UserRouter;
