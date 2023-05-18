b9021c0ee2578e8da5aba9ca7d4ea151f2f349cb
amal-prem <aldamal.prem@gmail.com>
2023-05-17 22:27:32 -0400

    adds tokens

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User from "../models/UserSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const UserRouter = express.Router();
 UserRouter.use(express.json());
@@ -17,8 +19,13 @@ UserRouter.get("/", async (req, res) => {
 UserRouter.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const user = await User.create({ name, email, password });
-    res.json({ msg: "user registered successfully", user: user });
+    if (name && email && password) {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({ name, email, password: hashPassword });
+      res.json({ msg: "user registered successfully", user: user });
+    } else {
+      res.status(400).json({ msg: "Please fill the required field" });
+    }
   } catch (error) {
     res.json({ error: error });
   }
@@ -27,11 +34,16 @@ UserRouter.post("/register", async (req, res) => {
 UserRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const existUser = await User.findOne({ email, password });
+    const existUser = await User.findOne({ email });
     if (!existUser) {
       res.status(404).json({ msg: "user not found" });
     }
-    res.status(201).json({ msg: "user logged in", user: existUser });
+    const comparePassword = await bcrypt.compare(password, existUser.password);
+    if (!comparePassword) {
+      res.status(400).json({ msg: "Wrong Credentials" });
+    }
+    const token = jwt.sign({ id: existUser._id }, process.env.SECRET);
+    res.status(201).json({ msg: "user logged in", token: token });
   } catch (error) {
     res.status(400).json({ error: error });
   }
