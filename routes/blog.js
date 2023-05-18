9e710be0a890c70590f3c4f329ffaa5874655175
amal-prem <aldamal.prem@gmail.com>
2023-05-18 08:42:14 -0400

    adds get route for blog

--- routes/blog.js
+++ routes/blog.js
@@ -4,4 +4,18 @@ import express from "express";
 const BlogRouter = express.Router();
 BlogRouter.use(express.json());
 
+const response = (res, status, result) => {
+  res.status(status).json(result);
+};
+
+BlogRouter.get("/", async (req, res) => {
+  await Blog.find()
+    .then((result) => {
+      response(res, 200, result);
+    })
+    .catch((err) => {
+      response(res, 400, { error: err });
+    });
+});
+
 export default BlogRouter;
